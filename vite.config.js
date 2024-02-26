5dfd63e423154e3f73af6f0726648a3dc452c64f
Zain B <zainbashir.srj@gmail.com>
2024-02-26 20:13:32 +0500

    updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import * as path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -12,7 +11,13 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['react-slidedown'],
+      external: ['react', 'react-dom', 'react-slidedown'], // Include react and react-dom as external dependencies
+      output: {
+        globals: {
+          react: 'React', // Provide global variables for React and react-dom
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 });
