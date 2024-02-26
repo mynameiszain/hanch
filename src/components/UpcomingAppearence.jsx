278d51fd867394ff554c0f3c2c617952265cff62
Zain B <zainbashir.srj@gmail.com>
2024-02-26 19:12:14 +0500

    new volume icon adjusted and animation

--- src/components/UpcomingAppearence.jsx
+++ src/components/UpcomingAppearence.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { SlideDown } from 'react-slidedown';
-import 'react-slidedown/lib/slidedown.css';
+
 import { eventData } from "../api/api";
 import { Link } from "react-router-dom";
 
